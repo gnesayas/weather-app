c3a69e6e2b24434027c848af1cd1484f75f0140a
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-10-06 13:31:11 -0500

    Add the initial html, css, and javascript files and link them

---
+++ javascript.js
@@ -0,0 +1,12 @@
+(function () {
+    const protocol = "https://"
+    const url = 'api.weatherapi.com/v1/current.json?';
+    const apiStart = "key=";
+    const apiKey = '';
+    const itemStart = "&q=";
+    const defaultLocation = "garland";
+    
+    function getWeather(location) {
+
+    }
+})();
