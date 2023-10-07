9a7549a653aac2db42a4724b7e1771ed518123e9
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-10-07 18:03:33 -0500

    Add background images for weather conditions and switch between them

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# weather-app
+This mini project is a weather app to help practice writing asynchronous
+code and working with json.
+
+Hompage image by <a href="https://pixabay.com/users/pexels-2286921/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1867317">Pexels</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1867317">Pixabay</a>
+
+Sunny image by Grooveland Designs: https://unsplash.com/photos/zjoydJb17mE
+
+Clear night image by Pixabay: https://www.pexels.com/photo/green-tree-268533/
+
+Partly cloudy image by Pixabay: https://www.pexels.com/photo/brown-field-and-blue-sky-46160/
+
+Cloudy image by Pixabay: https://www.pexels.com/photo/gray-rolled-asphalt-road-under-cloudy-sky-52531/
+
+Overcast image by <a href="https://unsplash.com/@antoinebarres?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Antoine Barrès</a> on <a href="https://unsplash.com/photos/o4u8pFwSiQE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Mist image by <a href="https://unsplash.com/@chrislawton?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Chris Lawton</a> on <a href="https://unsplash.com/photos/6tfO1M8_gas?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Rain image by <a href="https://unsplash.com/@eutahm?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Eutah Mizushima</a> on <a href="https://unsplash.com/photos/F-t5EpfQNpk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Snow image by Simon Berger: https://www.pexels.com/photo/photo-of-snowy-field-3462588/
+
+Sleet image by ArtHouse Studio: https://www.pexels.com/photo/dry-grass-covered-with-ice-in-sunny-day-4338012/
+
+Thunder image by Andre Furtado: https://www.pexels.com/photo/lightning-and-gray-clouds-1162251/
+
+Rain and thunder image by <a href="https://unsplash.com/@basilsmith?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Basil Smith</a> on <a href="https://unsplash.com/photos/KmsDi5XH__0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Snow and thunder image by <a href="https://pixabay.com/users/kellepics-4893063/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3412305">Stefan Keller</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3412305">Pixabay</a>
--- javascript.js
+++ javascript.js
@@ -26,7 +26,7 @@
     }
   }
 
-  function processInfo(response) {
+  function processResponse(response) {
     result = {};
     result.icon = response.current.condition.icon;
     result.text = response.current.condition.text;
@@ -46,15 +46,182 @@
     return result;
   }
 
+  const container = document.querySelector(".container");
+  const errorP = document.querySelector("p");
+  const footer = document.querySelector("h3");
+
   function processUserInput(location) {
     getWeather(location)
       .then(function (response) {
-        console.log(processInfo(response));
+        const result = processResponse(response);
+        console.log(result.text);
+        container.className = "container";
+        footer.replaceChildren();
+        footer.textContent = "Image by ";
+        const pictureLink = document.createElement("a");
+        if (result.text === "Sunny") {
+          container.classList.add("sunny");
+          pictureLink.setAttribute(
+            "href",
+            "https://unsplash.com/photos/zjoydJb17mE"
+          );
+          pictureLink.textContent = "Grooveland Designs";
+          footer.appendChild(pictureLink);
+        } else if (result.text === "Clear") {
+          container.classList.add("clear");
+          pictureLink.setAttribute(
+            "href",
+            "https://www.pexels.com/photo/green-tree-268533/"
+          );
+          pictureLink.textContent = "Pixabay";
+          footer.appendChild(pictureLink);
+        } else if (result.text === "Partly cloudy") {
+          container.classList.add("partly-cloudy");
+          pictureLink.setAttribute(
+            "href",
+            "https://www.pexels.com/photo/brown-field-and-blue-sky-46160/"
+          );
+          pictureLink.textContent = "Pixabay";
+          footer.appendChild(pictureLink);
+        } else if (result.text === "Cloudy") {
+          container.classList.add("cloudy");
+          pictureLink.setAttribute(
+            "href",
+            "https://www.pexels.com/photo/gray-rolled-asphalt-road-under-cloudy-sky-52531/"
+          );
+          pictureLink.textContent = "Pixabay";
+          footer.appendChild(pictureLink);
+        } else if (result.text === "Overcast") {
+          container.classList.add("overcast");
+          pictureLink.setAttribute(
+            "href",
+            "https://unsplash.com/photos/o4u8pFwSiQE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          );
+          pictureLink.textContent = "Antoine Barrès on Unsplash";
+          footer.appendChild(pictureLink);
+        } else if (
+          result.text === "Mist" ||
+          result.text === "Fog" ||
+          result.text === "Freezing fog"
+        ) {
+          container.classList.add("mist");
+          pictureLink.setAttribute(
+            "href",
+            "https://unsplash.com/photos/6tfO1M8_gas?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          );
+          pictureLink.textContent = "Chris Lawton on Unsplash";
+          footer.appendChild(pictureLink);
+        } else if (
+          result.text === "Patchy rain possible" ||
+          result.text === "Patchy freezing drizzle possible" ||
+          result.text === "Patchy light drizzle" ||
+          result.text === "Light drizzle" ||
+          result.text === "Freezing drizzle" ||
+          result.text === "Heavy freezing drizzle" ||
+          result.text === "Patchy light rain" ||
+          result.text === "Light rain" ||
+          result.text === "Moderate rain at times" ||
+          result.text === "Moderate rain" ||
+          result.text === "Heavy rain at times" ||
+          result.text === "Heavy rain" ||
+          result.text === "Light freezing rain" ||
+          result.text === "Moderate or heavy freezing rain" ||
+          result.text === "Light rain shower" ||
+          result.text === "Moderate or heavy rain shower" ||
+          result.text === "Torrential rain shower"
+        ) {
+          container.classList.add("rain");
+          pictureLink.setAttribute(
+            "href",
+            "https://unsplash.com/photos/F-t5EpfQNpk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          );
+          pictureLink.textContent = "Eutah Mizushima on Unsplash";
+          footer.appendChild(pictureLink);
+        } else if (
+          result.text === "Patchy snow possible" ||
+          result.text === "Blowing snow" ||
+          result.text === "Blizzard" ||
+          result.text === "Patchy light snow" ||
+          result.text === "Light snow" ||
+          result.text === "Patchy moderate snow" ||
+          result.text === "Moderate snow" ||
+          result.text === "Patchy heavy snow" ||
+          result.text === "Heavy snow" ||
+          result.text === "Light snow showers" ||
+          result.text === "Moderate or heavy snow showers"
+        ) {
+          container.classList.add("snow");
+          pictureLink.setAttribute(
+            "href",
+            "https://www.pexels.com/photo/photo-of-snowy-field-3462588/"
+          );
+          pictureLink.textContent = "Simon Berger";
+          footer.appendChild(pictureLink);
+        } else if (
+          result.text === "Patchy sleet possible" ||
+          result.text === "Light sleet" ||
+          result.text === "Moderate or heavy sleet" ||
+          result.text === "Ice pellets" ||
+          result.text === "Light sleet showers" ||
+          result.text === "Moderate or heavy sleet showers" ||
+          result.text === "Light showers of ice pellets" ||
+          result.text === "Moderate or heavy showers of ice pellets"
+        ) {
+          container.classList.add("sleet");
+          pictureLink.setAttribute(
+            "href",
+            "https://www.pexels.com/photo/dry-grass-covered-with-ice-in-sunny-day-4338012/"
+          );
+          pictureLink.textContent = "ArtHouse Studio";
+          footer.appendChild(pictureLink);
+        } else if (result.text === "Thundery outbreaks possible") {
+          container.classList.add("thunder");
+          pictureLink.setAttribute(
+            "href",
+            "https://www.pexels.com/photo/lightning-and-gray-clouds-1162251/"
+          );
+          pictureLink.textContent = "Andre Furtado";
+          footer.appendChild(pictureLink);
+        } else if (
+          result.text === "Patchy light rain with thunder" ||
+          result.text === "Moderate or heavy rain with thunder"
+        ) {
+          container.classList.add("rain-thunder");
+          pictureLink.setAttribute(
+            "href",
+            "https://unsplash.com/photos/KmsDi5XH__0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          );
+          pictureLink.textContent = "Basil Smith on Unsplash";
+          footer.appendChild(pictureLink);
+        } else if (
+          result.text === "Patchy light snow with thunder" ||
+          result.text === "Moderate or heavy snow with thunder"
+        ) {
+          container.classList.add("snow-thunder");
+          pictureLink.setAttribute(
+            "href",
+            "https://pixabay.com/users/kellepics-4893063/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3412305"
+          );
+          pictureLink.textContent = "Stefan Keller from Pixabay";
+          footer.appendChild(pictureLink);
+        }
+        errorP.textContent = "";
       })
       .catch(function (error) {
-        console.log(error);
+        if (error.message === "Parameter q is missing.") {
+          errorP.textContent = "Error: Empty search";
+        } else {
+          errorP.textContent = error;
+        }
       });
   }
 
-  processUserInput("garland");
+  const input = document.querySelector("input");
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    processUserInput(input.value);
+    input.value = "";
+  });
 })();
